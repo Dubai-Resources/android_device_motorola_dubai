2600da1f75ccdf35dc96c35b230b9772a8dafe5b
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2025-01-16 15:31:06 +0530

    dubai: Add Vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,27 @@
+# Common Device Tree
+rm -rf device/motorola/sm7325-common
+git clone https://github.com/Dubai-Resources/android_device_motorola_sm7325-common.git -b lineage-20 device/motorola/sm7325-common --depth=1
+
+# Vendor Tree
+rm -rf vendor/motorola/dubai
+git clone https://github.com/Dubai-Resources/proprietary_vendor_motorola_dubai.git -b lineage-20 vendor/motorola/dubai --depth=1
+
+# Common Vendor Tree
+rm -rf vendor/motorola/sm7325-common
+git clone https://github.com/Dubai-Resources/proprietary_vendor_motorola_sm7325-common.git -b lineage-20 vendor/motorola/sm7325-common --depth=1
+
+# Kernel Tree
+rm -rf kernel/motorola/sm7325
+git clone https://github.com/Dubai-Resources/android_kernel_motorola_sm7325.git -b 15.0-KSU kernel/motorola/sm7325 --depth=1
+
+# Hardware
+rm -rf hardware/motorola
+git clone https://github.com/LineageOS/android_hardware_motorola.git -b lineage-20 hardware/motorola
+
+# Viper4FX
+rm -rf packages/apps/ViPER4AndroidFX
+git clone https://github.com/Dheeraj3031A/packages_apps_ViPER4AndroidFX.git -b 13 packages/apps/ViPER4AndroidFX
+
+# BCR
+rm -rf vendor/bcr
+git clone https://github.com/Chaitanyakm/vendor_bcr.git -b main vendor/bcr --depth=1
