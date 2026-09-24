a7be4f124df7cb3ba9ec4d725dfaf928f1334604
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2026-09-24 22:58:43 +0530

    dubai: vendorsetup: Track new branches for 17.0
    Change-Id: Ief0899c202732a43f61931d98945b7fa32441254

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,19 +4,19 @@ git clone https://github.com/Dubai-Resources/android_device_motorola_sm7325-comm
 
 # Vendor Tree
 rm -rf vendor/motorola/dubai
-git clone https://github.com/Dubai-Resources/proprietary_vendor_motorola_dubai.git -b lineage-23.2 vendor/motorola/dubai --depth=1
+git clone https://github.com/Dubai-Resources/proprietary_vendor_motorola_dubai.git -b lineage-24.0 vendor/motorola/dubai --depth=1
 
 # Common Vendor Tree
 rm -rf vendor/motorola/sm7325-common
-git clone https://github.com/Dubai-Resources/proprietary_vendor_motorola_sm7325-common.git -b lineage-23.2 vendor/motorola/sm7325-common --depth=1
+git clone https://github.com/Dubai-Resources/proprietary_vendor_motorola_sm7325-common.git -b lineage-24.0 vendor/motorola/sm7325-common --depth=1
 
 # Kernel Tree
 rm -rf kernel/motorola/sm7325
-git clone https://github.com/Dubai-Resources/android_kernel_motorola_sm7325.git -b 16.0 kernel/motorola/sm7325 --depth=1
+git clone https://github.com/Dubai-Resources/android_kernel_motorola_sm7325.git -b 17.0 kernel/motorola/sm7325 --depth=1
 
 # Hardware
 rm -rf hardware/motorola
-git clone https://github.com/Dubai-Resources/hardware_motorola.git -b fifteen hardware/motorola
+git clone https://github.com/Dubai-Resources/hardware_motorola.git -b 17.0 hardware/motorola
 
 # BCR
 rm -rf vendor/bcr
